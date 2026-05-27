03f9fac22ba9c3d99d71504c4365cb8e62ae4b33
shawnseb <72581921+shawnseb@users.noreply.github.com>
2026-05-26 20:56:49 -0600

    Add: concatenation-of-array - submission-5

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-5.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans;
+        ans.reserve(nums.size()*2);
+        ans.insert(ans.end(), nums.begin(), nums.end());
+        ans.insert(ans.end(), nums.begin(), nums.end());
+        return ans;
+
+        
+    }
+};
