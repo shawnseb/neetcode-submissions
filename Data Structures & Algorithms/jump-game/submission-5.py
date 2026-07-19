fca3210f0787ec302170bbecb9dd323ea985d9cd
shawnseb <72581921+shawnseb@users.noreply.github.com>
2026-07-19 01:15:14 -0500

    Add: jump-game - submission-5

---
+++ Data Structures & Algorithms/jump-game/submission-5.py
@@ -0,0 +1,36 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        self.impossible = set()
+        return self.jumpHelp(nums, 0) == 1
+        
+    def jumpHelp(self, nums: List[int], index: int) -> int:
+        print(index)
+        if index > len(nums):
+            return 0
+        if index == len(nums) - 1:
+            return 1
+        if index in self.impossible:
+            return 0
+        actions = nums[index]
+        if actions == 0:
+            self.impossible.add(index)
+        sum = 0
+        print("hey")
+        feasible = 0
+        if (index + actions) >= len(nums):
+            feasible = len(nums) - index -1
+        else:
+            feasible = actions
+        for i in range(feasible, 0, -1):
+            print("hi")
+            sum = sum + self.jumpHelp(nums, index + i)
+            if sum == 1:
+                return 1
+        if sum == 0:
+            self.impossible.add(index)
+            return 0
+        return 1
+
+
+        
+        
