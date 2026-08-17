b79eac9763852a957edf7462613e652b8a462443
shawnseb <72581921+shawnseb@users.noreply.github.com>
2026-08-17 16:15:52 -0500

    Add: gas-station - submission-7

---
+++ Data Structures & Algorithms/gas-station/submission-7.py
@@ -0,0 +1,40 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+        def completeCircuitTry(index: int) -> (bool, int):
+            gasLeft = 0
+            start = index
+            run = True
+            while gasLeft >= 0 and (index!=start or run) :
+                run = False
+                gasLeft+=gas[index]
+                gasLeft-=cost[index]
+                index+=1
+                if index == len(gas):
+                    index = 0
+                
+              
+            if gasLeft < 0:
+               
+                return False, index
+                
+            else:
+    
+                return True, start
+        
+        start = 0
+        visited = -1
+        while start > visited:
+            visited = start
+            boolean, index = completeCircuitTry(start)
+            if boolean and index == start:
+                return index
+            start = index
+        return -1
+            
+            
+            
+
+        
+        
